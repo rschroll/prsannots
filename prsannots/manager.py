1ff940cf3d6d72cc13cbfb1f832dc871a16d48ee
Robert Schroll <rschroll@gmail.com>
2012-04-15 14:59:33 -0300

    Document manager.py

--- prsannots/manager.py
+++ prsannots/manager.py
@@ -29,6 +29,9 @@ class NotMountedError(Exception):
     pass
 
 class Manager(object):
+    """ Tracks a set of files on the eReader, so that annotated files can
+    be retrieved.
+    """
     
     _base_settings = { 'infix': 'annots', 'reader_dir': 'download', 'gs': False}
     _id_file = '.prsannots'
@@ -44,6 +47,11 @@ class Manager(object):
                 self.settings[key] = self._base_settings[key]
     
     def load(self, filename):
+        """ Load the configuration file specified by filename.  Note that
+        this method does not require the reader to be mounted.  The only
+        indication that it isn't will be that self.reader = None.
+        """
+        
         fd = open(filename, 'rb')
         self.settings = pickle.load(fd)
         self.library = pickle.load(fd)
@@ -55,6 +63,11 @@ class Manager(object):
             pass
     
     def load_if_mounted(self, filename):
+        """ Load the configuration file specified by filename if the
+        corresponding reader is mounted.  Return True if successful,
+        False otherwise.
+        """
+        
         fd = open(filename, 'rb')
         self.settings = pickle.load(fd)
         id_file = os.path.join(self.settings['mount'], self._id_file)
@@ -70,23 +83,44 @@ class Manager(object):
         return False
     
     def load_mounted_reader(self):
+        """ Load the reader that is mounted, if a configuration file is
+        found for it.  Returns True if a reader was found, False otherwise.
+        
+        Note that if multiple readers are mounted, only the "first" one
+        will be loaded.  Which one this is is deterministic, but will
+        seem random to the user.
+        """
+        
         for f in glob.glob(os.path.join(CONFIG_DIR, '*' + CONFIG_EXT)):
             if self.load_if_mounted(f):
                 return True
         return False
     
     def save(self):
+        """ Save the configuration. """
+        
         fd = open(os.path.join(CONFIG_DIR, self.settings['id'] + CONFIG_EXT), 'wb')
         pickle.dump(self.settings, fd, -1)
         pickle.dump(self.library, fd, -1)
         fd.close()
     
     def update_settings(self, **kw):
+        """ Update the global settings for the manager.
+        
+        Be sure to call save() sometime after this method.
+        """
+        
         for k,v in kw.items():
             if k in self.settings:
                 self.settings[k] = v
     
     def new(self, mount, **kw):
+        """ Create a new configuration.  mount is where the reader is
+        mounted.  Other keyword arguments specify global settings.
+        
+        Be sure to call save() sometime after this method.
+        """
+        
         mount = os.path.abspath(mount)
         if not os.path.ismount(mount):
             raise NotMountedError, "Reader does not appear to be mounted at %s." % mount
@@ -102,6 +136,32 @@ class Manager(object):
         self.update_settings(**kw)
     
     def add_pdf(self, filename, dice_pdf=None, dice_map=None, infix=None, reader_dir=None, gs=None):
+        """ Add a PDF file to the reader, to be managed by this manager.
+        
+        Inputs: filename    The location on the computer of the PDF file.
+                
+                dice_pdf    A pyPdf.PdfFileWriter with the diced PDF file
+                            to be put on the reader.  If None, then
+                            filename is copied to the reader.
+                
+                dice_map    The dice map describing how the original PDF
+                            was cut up for the reader.  None indicates
+                            no dicing occured.
+                
+                infix       The annotated PDF will be written back to the
+                            filesystem with the name filename.infix.pdf.
+                            If None, use the global settings.
+                
+                reader_dir  The directory on the reader to place the PDF
+                            file.  If None, use the global settings.
+                
+                gs          A Boolean flag on whether to run the diced
+                            PDF through Ghostscript.  If None, use the
+                            global settings.
+        
+        Be sure to call save() sometime after this method.
+        """
+        
         filename = os.path.abspath(filename)
         basename = os.path.basename(filename)
         if infix is None:
@@ -133,11 +193,37 @@ class Manager(object):
         self.library[relfn] = { 'filename': filename, 'infix': infix, 'annhash': 0, 'dice_map': dice_map }
     
     def add_diced_pdf(self, filename, diceargs, **kw):
+        """ Add a PDF file to the reader, diced as specified.
+        
+        Inputs: filename    The location on the computer of the PDF file.
+                
+                diceargs    The arguments to be passed to pdfdice.dice().
+                            As of this writing, a tuple of the form
+                            (ncols, nrows, [overlap]).  See pdfdice
+                            documentation for more details.
+                
+                Additional keyword arguments are the same as for add_pdf().
+        
+        Be sure to call save() sometime after this method.
+        """
+        
         pdf = pyPdf.PdfFileReader(open(filename, 'rb'))
         outpdf, dice_map = dice(pdf, *diceargs)
         self.add_pdf(filename, outpdf, dice_map, **kw)
     
     def sync_pdf(self, filepath):
+        """ Create an up-to-date annotated PDf for the specified file.
+        
+        This method short-circuits if the current anotated PDF is up-to-date.
+        
+        Input:  filepath    The location of the specified PDF file on the
+                            reader, relative to the mount point.
+        
+        Output: A Boolean indicating whether the annotated PDF was
+                updated or not.  If True, be sure to call save()
+                sometime in the future.
+        """
+        
         libentry = self.library[filepath]
         try:
             book = self.reader[filepath]
@@ -160,6 +246,12 @@ class Manager(object):
         return True
     
     def sync(self):
+        """ Sync all PDF files tracked my this manager.
+        
+        Returns the number of updated annotated PDF files.  Be sure to
+        call save() in the future if this number is > 0.
+        """
+        
         count = 0
         for f in self.library:
             if self.sync_pdf(f):
@@ -167,6 +259,21 @@ class Manager(object):
         return count
     
     def delete(self, reader_file=None, orig_file=None, delete_from_reader=False):
+        """ Remove a file from the manager, and optionally delete it from the reader.
+        
+        Inputs: reader_file         The path to the file on the reader,
+                                    relative to the mount point.
+                
+                orig_file           The path to the file on the computer.
+                                    Note that only one of reader_file
+                                    and orig_file should be specified.
+                
+                delete_from_reader  Whether to delete the PDF file from
+                                    the reader.
+        
+        Be sure to call save() sometime after this method.
+        """
+        
         if orig_file is not None:
             if reader_file is not None:
                 raise RuntimeError, "Specify only one of reader_file and orig_file."
@@ -185,7 +292,7 @@ class Manager(object):
             del(self.library[reader_file])
     
     def clean(self):
-        """ Remove files from library that are no longer on the Reader. """
+        """ Remove files from the manager that are no longer on the Reader. """
         for filepath in self.library.keys():
             if not os.path.exists(os.path.join(self.settings['mount'], filepath)):
                 self.delete(filepath)
